ab5a774a801e161bcc4f1bae7a4eb1f52405bea5
Jesus Ochoa <jesusochoa266@gmail.com>
2020-06-20 13:53:57 -0500

    ci: configuring package json enviroment file and removing a instruction in readme

--- front-angular/src/environments/environment.ts
+++ front-angular/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiLookBack: 'localhost:3000',
+  apiGitHub: 'https://api.github.com'
 };
 
 /*
