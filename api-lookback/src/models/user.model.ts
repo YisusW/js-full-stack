6a0e878befdb4c313d1a6dee1dfa5cdd54bba029
Jesus Ochoa <jesusochoa266@gmail.com>
2020-06-21 16:21:20 -0500

    feat: user controller model and repository created

---
+++ api-lookback/src/models/user.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  login: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  link: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  site_admin: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  type: string;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
